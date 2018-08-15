58640c286f6e5c96d95c83c5e52361a00e6b265a
Dorival Querino <dorival.querino@gmail.com>
2018-08-14 22:15:45 -0300

    First sources - Eureka and Customer microservice.

---
+++ eureka-server/src/main/java/com/cloud4java/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.cloud4java.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
