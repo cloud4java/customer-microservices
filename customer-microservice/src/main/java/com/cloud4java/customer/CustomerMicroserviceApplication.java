cee836a5f11a387cbdd28c86c30f80e283579e3a
Dorival Querino <dorival.querino@gmail.com>
2018-08-26 21:10:56 -0300

    Adjust of eureka server and client properties.

--- customer-microservice/src/main/java/com/cloud4java/customer/CustomerMicroserviceApplication.java
+++ customer-microservice/src/main/java/com/cloud4java/customer/CustomerMicroserviceApplication.java
@@ -3,9 +3,10 @@ package com.cloud4java.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class CustomerMicroserviceApplication {
 
 	public static void main(String[] args) {
--- customer-microservice/src/test/java/com/cloud4java/customer/JsonTest.java
+++ customer-microservice/src/test/java/com/cloud4java/customer/JsonTest.java
@@ -1,10 +1,11 @@
 package com.cloud4java.customer;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Main {
-	public static void main(String[] args) {
+public class JsonTest {
+	public static void main(String[] args) throws JSONException {
 
 		String str = "{\"elenco\":[\"Json Leigh\", \"Mary Stylesheet\", \"David Markupovny\"]}";
 		JSONObject json = new JSONObject(str);
