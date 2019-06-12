bb4c9ff3efcf0f5b9d80e812afcaa84301e11298
dorival.silva <dorival.querino@gmail.com>
2019-06-12 00:13:26 -0300

    Adde greeting method

--- customer-microservice/src/main/java/com/cloud4java/customer/CustomerController.java
+++ customer-microservice/src/main/java/com/cloud4java/customer/CustomerController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,10 +17,21 @@ public class CustomerController {
 	private RestTemplate restTemplate;
 	
 	@RequestMapping(path = "/get")
-	public void searchCustommer() {
+	public ResponseEntity<String> searchCustommer() {
 		log.info("Entering get method.....");
 		ResponseEntity<String> entity = restTemplate.getForEntity("http://jsonplaceholder.typicode.com/todos/1", String.class);
 		
 		log.info("Entering get method.....{}: ", entity);
+		
+		return entity;
+	}
+	
+	@RequestMapping(path = "/greetings/{name}")
+	public ResponseEntity<String> searchCustommer(@PathVariable("name") String name) {
+		ResponseEntity<String> entity = ResponseEntity.ok("Hello : " + name);
+		
+		log.info("Returning entity {}: ", entity);
+		
+		return entity;
 	}
 }
