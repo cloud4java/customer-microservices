d41743152200e063d3b8d4d0400c3fc7d286663a
Cloud4Java <42394614+cloud4java@users.noreply.github.com>
2019-06-12 20:38:07 -0300

    Update CustomerMicroserviceApplication.java
    Comment second main method.

--- customer-microservice/src/main/java/com/cloud4java/customer/CustomerMicroserviceApplication.java
+++ customer-microservice/src/main/java/com/cloud4java/customer/CustomerMicroserviceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class CustomerMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerMicroserviceApplication.class, args);
-	}
+	//public static void main(String[] args) {
+	//	SpringApplication.run(CustomerMicroserviceApplication.class, args);
+	//}
 }
